63c29b5367194601e76a1545f04a6a1b034003a8
thisotherguyalso <161091156+thisotherguyalso@users.noreply.github.com>
2025-02-13 19:29:44 +0800

    Add files via upload

---
+++ recipebook/ledger/views.py
@@ -0,0 +1,136 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def recipe_list(request):
+    context = {
+    "recipes": [
+        {
+            "name": "Recipe 1",
+            "ingredients": [
+                {
+                    "name": "tomato",
+                    "quantity": "3pcs"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1kg"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1L"
+                },
+                {
+                    "name": "sinigang mix",
+                    "quantity": "1 packet"
+                }
+            ],
+            "link": "/recipe/1"
+        },
+        {
+            "name": "Recipe 2",
+            "ingredients": [
+                {
+                    "name": "garlic",
+                    "quantity": "1 head"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "vinegar",
+                    "quantity": "1/2cup"
+                },
+                {
+                    "name": "water",
+                    "quanity": "1 cup"
+                },
+                {
+                    "name": "salt",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "whole black peppers",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1 kilo"
+                }
+            ],
+            "link": "/recipe/2"
+        }
+    ]
+}
+    return render(request, 'recipe_list.html', context)
+
+def recipe_1(request):
+    context = {
+    "name": "Recipe 1",
+    "ingredients": [
+        {
+            "name": "tomato",
+            "quantity": "3pcs"
+        },
+        {
+            "name": "onion",
+            "quantity": "1pc"
+        },
+        {
+            "name": "pork",
+            "quantity": "1kg"
+        },
+        {
+            "name": "water",
+            "quantity": "1L"
+        },
+        {
+            "name": "sinigang mix",
+            "quantity": "1 packet"
+        }
+    ],
+    "link": "/recipe/1"
+}
+    return render(request, 'recipe_1.html', context)
+
+def recipe_2(request):
+    context = {
+    "name": "Recipe 2",
+    "ingredients": [
+        {
+            "name": "garlic",
+            "quantity": "1 head"
+        },
+        {
+            "name": "onion",
+            "quantity": "1pc"
+        },
+        {
+            "name": "vinegar",
+            "quantity": "1/2cup"
+        },
+        {
+            "name": "water",
+            "quantity": "1 cup"
+        },
+        {
+            "name": "salt",
+            "quantity": "1 tablespoon"
+        },
+        {
+            "name": "whole black peppers",
+            "quantity": "1 tablespoon"
+        },
+        {
+            "name": "pork",
+            "quantity": "1 kilo"
+        }
+    ],
+    "link": "/recipe/2"
+}
+    return render(request, 'recipe_2.html', context)
