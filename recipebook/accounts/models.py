00d183cf33b3878994e4e99118671afb13972975
thisotherguyalso <161091156+thisotherguyalso@users.noreply.github.com>
2025-03-10 22:20:37 +0800

    Applied pep8 standards and fixed bio from Profile

--- recipebook/accounts/models.py
+++ recipebook/accounts/models.py
@@ -1,5 +1,7 @@
+"""Models configuration for accounts app"""
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 # Create your models here.
 
 
@@ -7,6 +9,5 @@ class Profile(models.Model):
     """Profile model for user"""
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True)
+    bio = models.TextField(validators=[MinLengthValidator(11)])
     name = models.CharField(max_length=50)
-    
--- recipebook/accounts/views.py
+++ recipebook/accounts/views.py
@@ -1,7 +1,9 @@
+"""Views configuration for accounts app"""
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
+
 class CustomView(LoginRequiredMixin, RecipeListView):
     template_name = "recipe_list.html"
-    redirect_field_name = '/login' # URL to redirect when not logged in
+    redirect_field_name = '/login'
